945ff47e74902bf32bd729de7832a4ccfa60c0c1
KB <yo.ht.challenge15@gmail.com>
2026-02-21 20:48:18 +0900

    Fix range in FizzBuzz from 1-30 to 1-31

--- level2/kadai2/kadai2-2.py
+++ level2/kadai2/kadai2-2.py
@@ -1,4 +1,4 @@
-for i in range(1, 30):
+for i in range(1, 31):
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
     elif i % 3 == 0:
@@ -6,4 +6,5 @@ for i in range(1, 30):
     elif i % 5 == 0:
         print("Buzz")
     else:
+
         print(i)
